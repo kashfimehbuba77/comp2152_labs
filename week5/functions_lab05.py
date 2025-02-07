2b2b72cdf4841c11de00901a624e927ce0eb82e6
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-02-07 08:39:45 -0500

    adding Q01

--- week5/functions_lab05.py
+++ week5/functions_lab05.py
@@ -1,8 +1,49 @@
 # Import the random library to use for the dice later
 import random
 
+# test
+
 # Will the line below print when you import function.py into main.py?
 # print("Inside function.py")
+
+
+# Lab 5: Question 4
+def use_loot():
+    good_loot_options = ["Health Potion", "Leather Boots"]
+    bad_loot_options = ["Poison Potion"]
+
+    print("    |    !!You see a monster in the distance! So you quickly use your first item:")
+    
+    if :
+        print("    |    You used " +  +" to up your health to " )
+    elif :
+        
+        print("    |    You used " +  + " to hurt your health to " + )
+    else:
+        print("    |    You used " +  + " but it's not helpful")
+    return 
+
+
+# Lab 5: Question 3 
+def collect_loot(loot_options, belt):
+    ascii_image3 = """
+                      @@@ @@                
+             *# ,        @              
+           @           @                
+                @@@@@@@@                
+               @   @ @% @*              
+            @     @   ,    &@           
+          @                   @         
+         @                     @        
+        @                       @       
+        @                       @       
+        @*                     @        
+          @                  @@         
+              @@@@@@@@@@@@          
+              """
+    print(ascii_image3)
+
+
 
 # Hero's Attack Function
 def hero_attacks(combat_strength, m_health_points):
@@ -69,3 +110,25 @@ def monster_attacks(m_combat_strength, health_points):
         health_points -= m_combat_strength
         print("    |    The monster has reduced Player's health to: " + str(health_points))
     return health_points
+
+# Lab 5: Question 7
+# Recursion
+# You can choose to go crazy, but it will reduce your health points by 5
+def inception_dream():
+
+    # Base Case
+
+        print("    |    You are in the deepest dream level now")
+        print("    |", end="    ")
+        input("Start to go back to real life? (Press Enter)")
+        print("    |    You start to regress back through your dreams to real life.")
+
+
+    # Recursive Case
+
+        # inception_dream(5)
+        # 1 + inception_dream(4)
+        # 1 + 1 + inception_dream(3)
+        # 1 + 1 + 1 + inception_dream(2)
+        # 1 + 1 + 1 + 1 + inception_dream(1)
+        # 1 + 1 + 1 + 1 + 2
