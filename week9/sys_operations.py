ed1f27f6b35e227bdbf7573663fbe97b08f76121
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-03-07 09:02:07 -0500

    adding txt file

--- week9/sys_operations.py
+++ week9/sys_operations.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import socket
 
@@ -12,6 +13,24 @@ print("===================")
 print("Set Socket Time Out to 50 Seconds")
 print(socket.setdefaulttimeout(50))
 print("Get the current Socket Time")
-print(socket.getdefaulttimeout(50))
+print(socket.getdefaulttimeout())
+print("===================")
+
+print("Get the current Operating System Type")
+print(os.name)
+print("Get the current operating system name: ")
+print(platform.system())
 
 print("===================")
+print("Current Process ID: ")
+print(os.getpid())
+
+file_name = "fdpractice.txt"
+print(f"\n[Before Forking] Process {os.getpid()} ")
+
+file_handle = os.open(file_name, os.O_RDWR | os.O_CREAT)
+print(f"\n[Process {os.getpid()} Opened file_handle: {file_handle} ")
+
+file_object_TextIO = os.fdopen(file_handle, "w+")
+
+file_object_TextIO = os.write("Some string blah blah")
