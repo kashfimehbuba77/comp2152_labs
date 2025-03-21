64ddd4f5287c5caf05249af2f5cc865c3f6c6399
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-03-21 08:30:44 -0400

    adding default constructor to person.py and getters & setters for p_name attribute

---
+++ week11/Person.py
@@ -0,0 +1,21 @@
+class Person:
+    #CONSTRUCTOR
+    def __init__(self, p_name, p_age, p_height):
+        print("Constructing the person object")
+        self.__name = p_name
+        self.__age = p_age
+        self.__height = p_height
+        self.public_prop = "I'm Public"
+
+        #Getter for name
+        @property 
+        def name(self):
+            return self.__name
+        
+        #Setter for name
+        @name.setter
+        def name(self, new_name):
+            self.__name = new_name
+
+        def __del__(self):
+            print("The garbage collector is automatically deleting the person object")
